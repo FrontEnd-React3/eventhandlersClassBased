11b41151102b968f51ba7b0abb35ffa684e17520
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2020-12-06 19:25:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Mybutton from "./Mybutton";
+import "./App.css";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button style={this.props.buttonStyle} onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+class ColorChanger extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blockColor: "#fff",
+      styling: {
+        padding: "10px 20px",
+        fontSize: 18,
+        borderRadius: 5
+      }
+    };
+  }
+
+  handleColorChange(color) {
+    this.setState({ blockColor: color });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className="ColorBlock"
+          style={{ width: 100, height: 100, background: this.state.blockColor }}
+        ></div>
+
+        <Button
+          onClick={() => this.handleColorChange("red")}
+          buttonStyle={this.state.styling}
+        >
+          Red
+        </Button>
+        <Button
+          onClick={() => this.handleColorChange("blue")}
+          buttonStyle={this.state.styling}
+        >
+          Blue
+        </Button>
+        <Button
+          onClick={() => this.handleColorChange("green")}
+          buttonStyle={this.state.styling}
+        >
+          Green
+        </Button>
+
+{/* I made it btn, doesn't work */}
+        <Mybutton
+        onClick={() => this.handleColorChange("green")}></Mybutton>
+      </div>
+
+    );
+  }
+}
+
+function App() {
+  const style = {
+    padding: "10px 20px",
+    fontSize: 18,
+    borderRadius: 5
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ColorChanger />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mybutton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Mybutton extends Component {
+  render() {
+    return (
+      <div>
+        <button>I made it</button>
+      </div>
+    );
+  }
+}
+
+export default Mybutton;
